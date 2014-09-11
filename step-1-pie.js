92a36faf8c3addb0598e673dc2f4ef5de9f7bf88
Kent English <kent.english@gmail.com>
2014-09-11 12:37:00 -0400

    Major overhaul

--- step-1-pie.js
+++ step-1-pie.js
@@ -1,31 +1,40 @@
 (function(d3) {
   'use strict';
 
-  var data = [10, 20, 30, 40];
+  var data = [
+    { count: 10 }, 
+    { count: 20 },
+    { count: 30 },
+    { count: 40 }
+  ];
 
-  var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
+  var width = 400;
+  var height = 400;
+  var radius = Math.min(width, height) / 2;
 
-  var color = d3.scale.category10();
+  var color = d3.scale.category20b();
 
   var arc = d3.svg.arc()
-    .outerRadius(radius - 10);
+    .outerRadius(radius);
 
-  var pie = d3.layout.pie() 
-    .value(function(d) { return d; });
+  var pie = d3.layout.pie()
+    .value(function(d) { return d.count; })
+    .sort(null);
 
-  var svg = d3.select('body')
+  var svg = d3.select('#chart')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
-    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+    .attr('transform', 'translate(' + (width / 2) + 
+      ',' + (height / 2) + ')');
 
   var path = svg.selectAll('path')
     .data(pie(data))
     .enter()
     .append('path')
     .attr('d', arc)
-    .attr('fill', function(d, i) { return color(i); });
+    .attr('fill', function(d, i) { 
+      return color(i); 
+    });
 })(window.d3);
